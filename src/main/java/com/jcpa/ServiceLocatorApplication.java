4817f1403e63621879918d7dc9b3a32bb22b229b
jonallen0 <jayayedev@gmail.com>
2016-11-10 13:35:45 +0000

    Initial commit from the atomist bot

---
+++ src/main/java/com/jcpa/ServiceLocatorApplication.java
@@ -0,0 +1,12 @@
+package com.jcpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceLocatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceLocatorApplication.class, args);
+    }
+}
---
+++ src/main/java/com/jcpa/ServiceLocatorConfiguration.java
@@ -0,0 +1,9 @@
+package com.jcpa;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ServiceLocatorConfiguration {
+    // Your beans go here ...
+    // Atomist Bot suggests "maybe use an Atomist project editor to add a sample one?"
+}
